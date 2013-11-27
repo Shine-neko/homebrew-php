f6cf37198bf45f9fbda9499e53525f060fe9dc04
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2013-11-27 16:58:55 +0100

    add sundown extension

---
+++ Formula/php55-sundown.rb
@@ -0,0 +1,22 @@
+require File.join(File.dirname(__FILE__), 'abstract-php-extension')
+
+class Php55Sundown < AbstractPhp55Extension
+  init
+  homepage 'https://github.com/chobie/php-sundown'
+  url 'https://github.com/chobie/php-sundown/archive/v0.3.8.zip'
+  sha1 'e510195ec7898d2d89b30bcbdf142f6e73dc3db1'
+  head 'https://github.com/chobie/php-sundown'
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    Dir.chdir 'ext/sundown' do
+      safe_phpize
+      system "./configure", "--prefix=#{prefix}",
+                            phpconfig
+      system "make"
+      prefix.install %w(modules/sundown.so)
+    end
+    write_config_file unless build.include? "without-config-file"
+  end
+end
